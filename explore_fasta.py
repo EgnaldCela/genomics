30ff2bffedf9fe97515fc7458e7038eb897d8ab0
Egnald <acelameta77@gmail.com>
2025-07-12 19:49:34 +0200

    added function read_file, potential issues

--- explore_fasta.py
+++ explore_fasta.py
@@ -6,6 +6,10 @@ DESCRIPTION
 A quick script made to navigate easily FASTA (.fna or .fasta) files, format typically used
 to store genomes after they're assembled.
 Typically they come compressed using the GNU zip format (.gz)
+
+IMPORTANT TIPS
+Always read .gz files with "rt" mode to treat them exactly as text files
+
 """
 # set it to your desired file
 filepath = "CHM13v2.0.fna.gz"
@@ -37,12 +41,56 @@ def read_headers(filepath, make_file=False, file_name=None):
     return headers
 
 
-def read_file(filepath, limit=None):
-    pass
+def read_file(filepath, limit=None, make_file=False):
+    """
+    INPUT:
+            filepath, path to fasta.gz file
+            limit, the number of bases you want to be read
+
+    OUTPUT:
+            out, list of tuples (header, bases)
+            (Optional) file with such content written
+    """
+    out = []
+    header = None
+    sequence = ""
+    
+    with gzip.open(filepath, "rt") as f:
+        for line in f:
+            line = line.strip()
+            # handle empty lines
+            if not line:
+                continue
+
+            # you either encounter the 1st header or consecutive ones
+            if line.startswith(">"):
+                if header and sequence:
+                    out.append((header, sequence))
+                if limit and len(sequence)>=limit:
+                    break
+                header = line
+                sequence = ""
+            else:
+                sequence += line
+                if len(sequence) >= limit:
+                    out.append((header, sequence[:limit]))
+    
+    if header and sequence and (not limit or len(sequence) <= limit):
+        out.append((header, sequence))
+    
+    if make_file:
+        with open("genome_reading.txt", "w") as f_out:
+            for (header, content) in out:
+                f_out.write(header + "\n")
+                f_out.write(content + "\n")
+
+    return out
+                
+
+
 
 if __name__ == '__main__':
-    print(read_headers(filepath, make_file=True, file_name="test_headers"))
-
+    print(read_file(filepath, limit=100))
 
 """
 SIDE NOTES:
